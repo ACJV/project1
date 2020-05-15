58bd08bd461d30e5fd3b42a310126668223540d4
justedilyte <64971805+justedilyte@users.noreply.github.com>
2020-05-15 17:05:13 +0200

    Added a VERY serious comment

--- project/src/main/java/com/example/project/ProjectApplication.java
+++ project/src/main/java/com/example/project/ProjectApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(ProjectApplication.class, args);
+        // A SERIOUS COMMENT !!!!!!!
+        // PERIOD.
     }
 
 }
