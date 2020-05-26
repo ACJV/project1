63d1fb57d2d54b271287f4b65b1ebcfebb2ab54d
npsyco <christian.nymark@gmail.com>
2020-05-26 19:06:46 +0200

    html updated

--- project/src/main/java/com/example/project/Controller/HomeController.java
+++ project/src/main/java/com/example/project/Controller/HomeController.java
@@ -3,12 +3,14 @@ package com.example.project.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*
 @Controller
 public class HomeController
 {
     @GetMapping("/")
     public String vehicle()
-       return "home/vehicle";
+       return "home/vehicle"
 
 
 }
+*/
