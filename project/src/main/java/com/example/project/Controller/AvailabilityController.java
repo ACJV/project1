01f634f6b41dd4bff8c0d0f31f84dcbd9dd02fca
thori <a.bragason@gmail.com>
2020-06-04 01:07:38 +0200

    Finished commenting the parts of code that I made. Did not comment model or service since I believe their purpose is evident. Methods that don't include comments are most likely comments that say what they do in the method name.

--- project/src/main/java/com/example/project/Controller/AvailabilityController.java
+++ project/src/main/java/com/example/project/Controller/AvailabilityController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 
 /* @Author: Ástþór Arnar Bragason
-* @Author2: Justé
+* Contributing author for method called below: Justé
 *
 * Availability Controller's main purpose is to receive date input by user and to utilize the Availability Repository
 * to check which vehicles are available and unavailable for the selected period.
@@ -40,62 +40,73 @@ public class AvailabilityController {
     DataManipulation dataManipulation;
 
 //----------------------------------------------------------------------------------------------------------------------
-    //@Ástþór Arnar
+    //@Ástþór Arnar (unless otherwise mentioned for specific method)
 //----------------------------------------------------------------------------------------------------------------------
 
-    // Availability:
+
+    // Availability: Create Booking Step 1
     @GetMapping("/availability")
     public String availability(Model model, String startDate, String endDate){
-        // Juste
+        // @Juste:
         model.addAttribute("today", dataManipulation.getTodaysDate());
-        // Ástþór:
-        // Checks if there is startDate and endDate have been selected by
+        // @Ástþór:
+        // Checks if there is startDate and endDate have been selected by user,
         if(startDate != null && endDate != null) {
+            // Uses isDate method (Bottom of this class - returns boolean value)
+            // to use try catch method and parse string to LocalDate to see if they are dates.
             if(isDate(startDate, endDate)) {
+                // uses modelAttribute to display array of available and unavailable vehicles.
                 model.addAttribute("AvailableVehicles", availabilityService.fetchAvailabilityVehicles(startDate, endDate, "Available"));
                 model.addAttribute("UnAvailableVehicles", availabilityService.fetchAvailabilityVehicles(startDate, endDate, "Unavailable"));
-                //Vehicle chosen = vehicleService.findVehicle(regNumber);
+                // Instantiate booking object, setting pickUp and dropOff date:
                 Booking tester = new Booking();
                 tester.setPickUpDate(startDate);
                 tester.setDropOffDate(endDate);
-                //tester.setVehicleRegNumber(chosen.getRegNumber());
+                // Creates list for single booking to use dates selected as PathVariable for GetMapping method below.
                 List<Booking> selected = new ArrayList<>();
                 selected.add(tester);
+                // Model to display and use selected dates.
                 model.addAttribute("datesL", selected);
             }
+            // Returns availability page
             return "home/Availability/availability";
-        } else {
+        } else { // if not dates - Will call simple error page and link to go back to page - should never happen
           return "home/Availability/availability";
         }
     }
+
+    // Create booking step 2:
+    // uses startDate, endDate and regNumber from available vehicle as PathVariables to instantiate booking.
     @GetMapping("/dates/start/{startDate}/end/{endDate}/reg/{regNumber}")
-    public String dates (@PathVariable("startDate") String startDate, @PathVariable("endDate") String endDate, @PathVariable("regNumber") String regNumber){
+    public String dates (@PathVariable("startDate") String startDate,
+                         @PathVariable("endDate") String endDate,
+                         @PathVariable("regNumber") String regNumber){
+        // Although it shouldn't be possible for repository to return a null value,
+        // this was added just in case - would be removed in future iterations while condoning unit testing.
         Vehicle chosen = vehicleService.findVehicle(regNumber);
         if(!chosen.equals(null)){
+            // Instantiate booking, set dates and office addressId for pickUp and dropOff locations
             Booking b = new Booking();
             b.setRegNumber(regNumber);
             b.setPickUpDate(startDate);
             b.setDropOffDate(endDate);
             b.setPickUpId(1);
             b.setDropOffId(1);
-
+            // Calculate total price
             b.setTotalPrice(dataManipulation.calculateTotalPriceConfirmed(b));
-
+            // Add booking to database
             bookingService.addBooking(b);
+            // Find bookingNumber by checking for values set
             Booking booking = bookingService.findBookingNumber(b);
+            // Booking Number used to pass on booking information for next method, using PathVariable
             int bookingNo = booking.getBookingNo();
+            // redirects to findCustomerForBooking - Method found in BookingController.
             return "redirect:/findCustomerForBooking/"+bookingNo;
         } else {
             return "home/Availability/error";
         }
     }
-
-
-//----------------------------------------------------------------------------------------------------------------------
-    //@Juste
-//----------------------------------------------------------------------------------------------------------------------
-
-
+    // isDate takes two strings, uses try/catch and returns boolean value set to default true except if exception occurs
     public boolean isDate (String startDate, String endDate) {
         boolean dates = true;
         try{
--- project/src/main/java/com/example/project/Controller/CustomerController.java
+++ project/src/main/java/com/example/project/Controller/CustomerController.java
@@ -23,40 +23,75 @@ public class CustomerController {
     @Autowired
     AddressService addressService;
 
-    // Should Be done.
+//----------------------------------------------------------------------------------------------------------------------
+    //@Ástþór
+//----------------------------------------------------------------------------------------------------------------------
+
+    // Gets all customers and searches for keyword in customer name if not null
     @GetMapping("/customer")
     public String customer(Model model, String keyword) {
+        // Fetches all customers
         List<Customer> customerList = customerService.fetchAll();
-
+        // if keyword is not null
         if(keyword != null){
+            // Search customer name for keyword and display customer matching keyword input by user
             model.addAttribute("customers", customerService.findByKeyword(keyword));
         } else {
+            // if keyword is null - display all customers
             model.addAttribute("customers", customerList);
         }
-
+        // returns customer page
         return "home/Customers/customer";
     }
 
-
-
-    @GetMapping("/create")
-    public String create(){
-        return "home/Customers/create";
-    }
-
-    @PostMapping("/create")
-    public String create(@ModelAttribute Customer customer){
-        customerService.addCustomer(customer);
-        return "redirect:/customer";
-    }
-
+    // Gets view customer page with pathVariable customerId to fetch customer in database.
     @GetMapping("/viewCustomer/{customerId}")
     public String viewCustomer(@PathVariable("customerId") int customerId, Model model) {
-        System.out.println("GetMapping /viewCustomer, test ID=5: " + customerId);
         model.addAttribute("customer", customerService.findCustomer(customerId));
         return "home/Customers/viewCustomer";
     }
 
+    // delete customer using pathvariable to locate customer in database
+    @GetMapping("/deleteCustomer/{customerId}")
+    public String delete(@PathVariable("customerId") int customerId){
+        boolean deleted = customerService.deleteCustomer(customerId);
+        // redirects to customer page
+        return "redirect:/customer";
+    }
+
+    // updated create customer page, that creates address at the same time and links customer to address table in db
+    @GetMapping("/createAddressAndCustomer")
+    public String createAddressAndCustomer(){
+        return "home/Customers/createAddressAndCustomer";
+    }
+
+    // Using model attributes to instantiate customer and address, adding to database and linking together
+    @PostMapping("/createAddressAndCustomer")
+    public String createAddressAndCustomer(@ModelAttribute Address address, @ModelAttribute Customer customer){
+        // add address to database
+        addressService.addAddress(address);
+        // find address id
+        Address a = addressService.findAddressId(address);
+        int addressId = a.getAddressId();
+        // set address id to customer, to link customer to correct address.
+        customer.setAddressId(addressId);
+        // add customer to database
+        customerService.addCustomer(customer);
+        // redirecting to customer page
+        return "redirect:/customer";
+    }
+
+    // saving customer used in view customer page to update customer
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute Customer customer){
+        customerService.updateCustomer(customer.getCustomerId(), customer);
+        return "redirect:/customer";
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Methods not in use in current system
+
+    // updateCustomer page which is not in use
     @GetMapping("/updateCustomer/{customerId}")
     public String updateCategory(@PathVariable("customerId") int customerId, Model model)
     {
@@ -64,37 +99,17 @@ public class CustomerController {
         model.addAttribute("customer", customer);
         return "home/Customers/updateCustomer";
     }
-
-
-    @GetMapping("/deleteCustomer/{customerId}")
-    public String delete(@PathVariable("customerId") int customerId){
-
-        boolean deleted = customerService.deleteCustomer(customerId);
-        if(deleted){
-            return "redirect:/customer";
-        } else {
-            return "redirect:/customer";
-        }
+    // Gets create customer html page
+    @GetMapping("/create")
+    public String create(){
+        return "home/Customers/create";
     }
 
-    @GetMapping("/createAddressAndCustomer")
-    public String createAddressAndCustomer(){
-        return "home/Customers/createAddressAndCustomer";
-    }
-
-    @PostMapping("/createAddressAndCustomer")
-    public String createAddressAndCustomer(@ModelAttribute Address address, @ModelAttribute Customer customer){
-        addressService.addAddress(address);
-        Address a = addressService.findAddressId(address);
-        int addressId = a.getAddressId();
-        customer.setAddressId(addressId);
+    // uses model attribute to instantiate customer and save to database
+    @PostMapping("/create")
+    public String create(@ModelAttribute Customer customer){
         customerService.addCustomer(customer);
-        return "redirect:/customer";
-    }
-
-    @PostMapping("/saveCustomer")
-    public String saveCustomer(@ModelAttribute Customer customer){
-        customerService.updateCustomer(customer.getCustomerId(), customer);
+        // redirects to customer page
         return "redirect:/customer";
     }
 
